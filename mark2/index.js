3814c0541db1dccb36cdab2f6ccd378450546e74
Anushka Gupta <anushkagupta645@gmail.com>
2021-09-24 15:46:08 +0530

    added one more question in questions

--- mark2/index.js
+++ mark2/index.js
@@ -4,7 +4,7 @@ console.log(chalk.magentaBright.bold("Welcome to Harry Potter quiz"));
 var username=readlineSync.question(chalk.greenBright("Enter Username: "));
 console.log(chalk.yellow("Hi "+username+", Lets begin the quiz!"));
 
-let questions=[{question:"In what house did the Sorting Hat almost put Harry?\na:Gryfindor b:Slytherin c:Hufflepuff\n",answer:"b"},{question:"Who were Harry's parents?\na:William and Elizabeth Potter b:James and Lily Potter\n",answer:"b",},{question:"What is Tom Riddle's middle name?\na:Lucius b:Salazar c:Marvolo\n",answer:"c"},{question:"How did Harry and Ron get to Hogwarts their second year?\na:flying car b:broomsticks\n",answer:"a"}];
+let questions=[{question:"In what house did the Sorting Hat almost put Harry?\na:Gryfindor b:Slytherin c:Hufflepuff\n",answer:"b"},{question:"What is Tom Riddle's middle name?\na:Lucius b:Salazar c:Marvolo\n",answer:"c"},{question:"How did Harry and Ron get to Hogwarts their second year?\na:flying car b:broomsticks\n",answer:"a"},{question:"Who were Harry's parents?\na:William and Elizabeth Potter b:James and Lily Potter\n",answer:"b"},{question:"What is the name of Hagrid’s three headed dog?\na:fluffy b:Buckbeak c:Aragog\n",answer:"a"}];
 let score=0;
 let highscore=[{name:"aashi",score:"0"}];
 function play(){
